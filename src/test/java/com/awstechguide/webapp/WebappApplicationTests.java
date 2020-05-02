192bde8384ef2d7be04b4b3b81bfa8ead55b9825
ObliqueOne <60855535+obliqueone-git@users.noreply.github.com>
2020-05-01 20:44:08 -0400

    initial commit

---
+++ src/test/java/com/awstechguide/webapp/WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awstechguide.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
