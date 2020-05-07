9666ee89ea004899b85ea8ba970c644018c4e43e
AWSTechGuide <info@awstechguide.com>
2020-05-07 17:02:11 -0400

    add functional and acceptance test projects

---
+++ acceptance-test/src/main/java/com/awstechguide/automation/App.java
@@ -0,0 +1,13 @@
+package com.awstechguide.automation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
