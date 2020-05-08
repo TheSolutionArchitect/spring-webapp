1571bbaa0e39177fb9a38bcb60e98e1dab6983bd
AWSTechGuide <info@awstechguide.com>
2020-05-08 14:18:56 -0400

    functional test code

--- functional-test/src/test/java/com/awstechguide/automation/AppTest.java
+++ functional-test/src/test/java/com/awstechguide/automation/AppTest.java
@@ -1,8 +1,11 @@
 package com.awstechguide.automation;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 /**
  * Unit test for simple App.
@@ -12,9 +15,25 @@ public class AppTest
     /**
      * Rigorous Test :-)
      */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+	private WebDriver driver;
+	String URL = "http://google.com";
+	
+	@BeforeClass
+	public void testSetUp() {	
+		System.setProperty("webdriver.chrome.driver","C:\\MyLocal\\AWSTechGuide\\workspace\\chromedriver_win32\\chromedriver.exe");
+		driver = new ChromeDriver();
+	}
+	
+	@Test
+	public void verifyGooglePageTittle() {
+		driver.navigate().to(URL);
+		String getTitle = driver.getTitle();
+		Assert.assertEquals(getTitle, "Google");
+		System.out.println("getTitle*********"+getTitle);
+	}
+	
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
+	}
 }
