1c413d970dc349a6f8ea2c43ec906d2b1c24f7de
AWS Tech Guide <62712515+awstechguide@users.noreply.github.com>
2020-05-11 16:28:51 -0400

    Update AppTest.java

--- acceptance-test/src/test/java/com/awstechguide/automation/AppTest.java
+++ acceptance-test/src/test/java/com/awstechguide/automation/AppTest.java
@@ -19,8 +19,8 @@ public class AppTest
 {
 
 	public HtmlUnitDriver driver;
-	public String HOME_URL = "http://54.86.45.254:8080/webapptest/home";
-	public String PRODUCTS_URL = "http://54.86.45.254:8080/webapptest/products";
+	public String HOME_URL = "http://3.93.190.172:8080/webapptest/home";
+	public String PRODUCTS_URL = "http://3.93.190.172:8080/webapptest/products";
 	
 	@Test
 	public void test1() {
