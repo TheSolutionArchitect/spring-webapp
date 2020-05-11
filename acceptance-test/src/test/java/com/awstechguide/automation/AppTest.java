332cd434d01ec14a653d00575962a9bdb6864ee9
AWSTechGuide <info@awstechguide.com>
2020-05-11 11:02:01 -0400

    acceptance test

--- acceptance-test/src/test/java/com/awstechguide/automation/AppTest.java
+++ acceptance-test/src/test/java/com/awstechguide/automation/AppTest.java
@@ -2,12 +2,14 @@ package com.awstechguide.automation;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 /**
@@ -17,34 +19,24 @@ public class AppTest
 {
 
 	public HtmlUnitDriver driver;
-	//public WebDriver driver;
-	public WebDriverWait wait;
 	public String URL = "http://google.com";
 	public String URL2 = "http://awstechguide.com";
 	
-	@Test(priority = 0)
-    public void testUrl()  {
-		driver = new HtmlUnitDriver();
-		//driver= new FirefoxDriver();
-		wait = new WebDriverWait(driver, 5);
-		driver.get(URL);
-		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-		String getTitle = driver.getTitle();
-		Assert.assertEquals(getTitle, "Google");
-		driver.findElement(By.xpath("//input[@type='text']")).sendKeys("Som Speaks");;
-		driver.findElement(By.xpath("//input[@value='Google Search']")).click();;
-		System.out.println("getTitle*********"+getTitle);
-		driver.quit();
-    }
+	@Test
+	public void test1() {
+		   driver = new HtmlUnitDriver();
+		   driver.navigate().to(URL2);
+		   List<WebElement> el= driver.findElements(By.xpath("//*[text()[contains(.,'AWS Tech')]]"));
+		   System.out.println("No of existance: "+el.size());
+		   driver.findElements(By.partialLinkText("Tech"));
+	   }
 	
-	@Test(priority = 1)
-    public void testUrl2()  {
-		driver = new HtmlUnitDriver();
-		wait = new WebDriverWait(driver, 5);
-		driver.get(URL2);
-		String getTitle = driver.getTitle();
-		Assert.assertEquals(getTitle, "AWS Tech Guide");
-		System.out.println("getTitle*********"+getTitle);
-		driver.quit();
-	}
+	@Test
+	public void test2() {
+		   driver = new HtmlUnitDriver();
+		   driver.navigate().to(URL2);
+		   List<WebElement> el= driver.findElements(By.xpath("//*[text()[contains(.,'AWS Tech')]]"));
+		   System.out.println("No of existance: "+el.size());
+		   driver.findElements(By.partialLinkText("Tech"));
+	   }
 }
