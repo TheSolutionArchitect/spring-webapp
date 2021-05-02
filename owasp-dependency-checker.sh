68e88e0e3ac0accf46e009b4f8a0c28e0719051e
AWS Tech Guide <62712515+awstechguide@users.noreply.github.com>
2021-05-01 21:39:24 -0400

    Update owasp-dependency-checker.sh

--- owasp-dependency-checker.sh
+++ owasp-dependency-checker.sh
@@ -24,6 +24,4 @@ docker run --rm \
     --volume $(pwd):/src:z \
     --volume "$DATA_DIRECTORY":/usr/share/dependency-check/data:z \
     --volume $(pwd)/odc-reports:/report:z \
-    owasp/dependency-check:$DC_VERSION --scan /src --format "ALL" --project "$DC_PROJECT" --out /report
-    # Use suppression like this: (where /src == $pwd)
-    # --suppression "/src/security/dependency-check-suppression.xml"
+    owasp/dependency-check:latest --scan /src --format "ALL" --project "$DC_PROJECT" --out /report
