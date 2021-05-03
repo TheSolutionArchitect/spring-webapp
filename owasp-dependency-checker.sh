9259b67caccc167dfc9d4e3e89e98325ef021d20
AWS Tech Guide <62712515+awstechguide@users.noreply.github.com>
2021-05-02 20:45:15 -0400

    Update owasp-dependency-checker.sh

--- owasp-dependency-checker.sh
+++ owasp-dependency-checker.sh
@@ -1,29 +1,33 @@
 #!/bin/sh
 
-OWASPDC_DIRECTORY=$HOME/OWASP-Dependency-Check
-DATA_DIRECTORY="$OWASPDC_DIRECTORY/data"
-REPORT_DIRECTORY="$OWASPDC_DIRECTORY/reports"
+DC_VERSION="latest"
+DC_DIRECTORY=$HOME/OWASP-Dependency-Check
+DC_PROJECT="dependency-check scan: $(pwd)"
+DATA_DIRECTORY="$DC_DIRECTORY/data"
+CACHE_DIRECTORY="$DC_DIRECTORY/data/cache"
 
 if [ ! -d "$DATA_DIRECTORY" ]; then
-    echo "Initially creating persistent directories"
+    echo "Initially creating persistent directory: $DATA_DIRECTORY"
     mkdir -p "$DATA_DIRECTORY"
-    chmod -R 777 "$DATA_DIRECTORY"
-
-    mkdir -p "$REPORT_DIRECTORY"
-    chmod -R 777 "$REPORT_DIRECTORY"
+fi
+if [ ! -d "$CACHE_DIRECTORY" ]; then
+    echo "Initially creating persistent directory: $CACHE_DIRECTORY"
+    mkdir -p "$CACHE_DIRECTORY"
 fi
 
 # Make sure we are using the latest version
-docker pull owasp/dependency-check
+docker pull owasp/dependency-check:$DC_VERSION
 
 docker run --rm \
-    --volume $(pwd):/src \
-    --volume "$DATA_DIRECTORY":/usr/share/dependency-check/data \
-    --volume "$REPORT_DIRECTORY":/report \
-    owasp/dependency-check \
+    -e user=$USER \
+    -u $(id -u ${USER}):$(id -g ${USER}) \
+    --volume $(pwd):/src:z \
+    --volume "$DATA_DIRECTORY":/usr/share/dependency-check/data:z \
+    --volume $(pwd)/odc-reports:/report:z \
+    owasp/dependency-check:$DC_VERSION \
     --scan /src \
     --format "ALL" \
-    --project "My OWASP Dependency Check Project" \
+    --project "$DC_PROJECT" \
     --out /report
-    # Use suppression like this: (/src == $pwd)
+    # Use suppression like this: (where /src == $pwd)
     # --suppression "/src/security/dependency-check-suppression.xml"
